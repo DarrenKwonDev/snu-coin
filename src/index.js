6891d5e632a4db1c3496ddfb6a2cc36c99e1ed53
DarrenKwonDev <anatomy1545@gmail.com>
2021-06-10 03:53:47 +0900

    debounce, throttle

--- src/index.js
+++ src/index.js
@@ -2,21 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./style/reset.css";
-import styled, { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import theme from "./theme/theme";
 import GlobalStyle from "./theme/globalStyle";
-
-const S = {
-  GlobalWrapper: styled.div``,
-};
 
 ReactDOM.render(
   <>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <S.GlobalWrapper>
-        <App />
-      </S.GlobalWrapper>
+      <App />
     </ThemeProvider>
   </>,
   document.getElementById("root")
